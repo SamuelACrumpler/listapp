930d5063f9fce414b166f429275ce392be6b1f9c
SamuelACrumpler <samuel.a.crumpler@gmail.com>
2020-02-28 12:47:41 -0800

    Fleshed Out Main JS a little
    Added HTML to Main..JS to get an idea of how it is going to look. Need to figure out adding rows to the table as well as making a proper drop down box

--- src/Main.js
+++ src/Main.js
@@ -11,8 +11,47 @@ import './css/login.css';
 import food from './js/classes/food.js';
 
 function Main() {
+
+	//https://codepen.io/cfmatre/pen/pgdLoR to install a proper combo box
+
 	var apple = new food(1, "apple", "fruit", 5);
 	apple.print();
+
+	return (
+		<div className="container">
+			<div className="row">
+				<div className="col-6">
+					<input type="text" placeholder="Username" className="form-control" />
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+				</div>
+			</div>
+			<div className="row">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Food</th>
+                            <th scope="col">Type</th>
+                            <th scope="col">Rating</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Apple</td>
+                            <td>Fruit</td>
+                            <td>3</td>
+                        </tr>
+                    </tbody>
+                </table>
+			</div>
+		</div>
+		
+		);
 }
 
 export default Main;
