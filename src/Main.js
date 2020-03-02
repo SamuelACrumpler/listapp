918905e2bac4aa694092f3ea774e3fa80c3122cf
SamuelACrumpler <samuel.a.crumpler@gmail.com>
2020-03-02 09:45:19 -0800

    Added notes to Main.Js
    Just created some notes for properly updating the table

--- src/Main.js
+++ src/Main.js
@@ -10,6 +10,78 @@ import React, { Component } from 'react';
 import './css/login.css';
 import food from './js/classes/food.js';
 
+
+//turn this into a class
+
+
+class Main extends Component {
+	constructor(props) {
+		super(props)
+
+
+		this.state = {
+			list: [],
+		};
+	}
+
+	render() {
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="col-6">
+						<input type="text" placeholder="Username" className="form-control" />
+					</div>
+					<div className="col">
+
+					</div>
+					<div className="col">
+					</div>
+				</div>
+				<div className="row">
+					<table class="table">
+						<thead>
+							<tr>
+								<th scope="col">ID</th>
+								<th scope="col">Food</th>
+								<th scope="col">Type</th>
+								<th scope="col">Rating</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<th scope="row">1</th>
+								<td>Apple</td>
+								<td>Fruit</td>
+								<td>3</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		);
+	}
+}
+
+function AddRow(i,n,t,r) {
+
+	//get id, name, type, rating by ids
+	//Push those values into states
+
+	//https://www.robinwieruch.de/react-state-array-add-update-remove
+
+
+	//https://reactjs.org/docs/faq-state.html for updating the state
+
+	//https://stackoverflow.com/questions/36651583/dynamically-add-child-components-in-react info to properly add new elements by updating the state of react
+
+	
+	
+	this.setState((state) => {
+		// Important: read `state` instead of `this.state` when updating.
+		return { list : state.count + 1 }
+	});
+}
+ 
 function Main() {
 
 	//https://codepen.io/cfmatre/pen/pgdLoR to install a proper combo box
